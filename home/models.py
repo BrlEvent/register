7243867650cedda353c677bd4669bf9c62af70c3
Naitik <naitik12gupta05@gmail.com>
2024-12-07 23:21:48 +0530

    Changed requirements.txt

--- home/models.py
+++ home/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+class GenderType(models.TextChoices):
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
 # Create your models here.
 class User(models.Model):
     fullname = models.CharField(max_length=30)
     branch = models.CharField(max_length=50)
-    gender = models.CharField(max_length=6,default='MALE')
-    hostel=models.CharField(max_length=5,default='YES')
     student_no = models.CharField(max_length=10,unique=True)
     email = models.EmailField(unique=True)
     phone_no = models.IntegerField()
+    gender = models.CharField(max_length=20,choices=GenderType.choices,null=True)
+    hostel=models.CharField(max_length=5,default='NO')
     attendance=models.BooleanField(default=False)
 
