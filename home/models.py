0dcbb66fa6df6f343918398575d091fcb5c5bce4
Naitik <naitik12gupta05@gmail.com>
2024-12-07 22:53:11 +0530

    Added all validations

--- home/models.py
+++ home/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     fullname = models.CharField(max_length=30)
-    branch = models.CharField(max_length=10)
-    student_no = models.CharField(max_length=20,unique=True)
+    branch = models.CharField(max_length=50)
+    gender = models.CharField(max_length=6,default='MALE')
+    hostel=models.CharField(max_length=5,default='YES')
+    student_no = models.CharField(max_length=10,unique=True)
     email = models.EmailField(unique=True)
     phone_no = models.IntegerField()
+    attendance=models.BooleanField(default=False)
 
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from .models import User
 from .serializers import userSerializer
+import re
 
 
 class Signup(APIView):
@@ -14,6 +15,28 @@ class Signup(APIView):
             student_no = request.data.get('student_no')
             email = request.data.get('email')
             phone_no = request.data.get('phone_no')
+            gender = request.data.get('gender')
+            hostel=request.data.get('hostel')
+
+            student_no_pattern = r"^\d{6,9}$"
+            if not re.match(student_no_pattern, student_no):
+                return JsonResponse(
+                    {"msg": "Invalid student number. Must be between 6 and 9 digits."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            email_pattern = r"^[a-zA-Z]+\d+@akgec\.ac\.in$"
+            if not re.match(email_pattern, email):
+                return JsonResponse(
+                    {"msg": "Invalid email format. Must be (name)(number)@akgec.ac.in"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            phone_pattern = r"^(?:\+91)?[6-9]\d{9}$"
+            if not re.match(phone_pattern, phone_no):
+                return JsonResponse(
+                    {"msg": "Invalid phone number. Must be 10 digits or start with +91 followed by 10 digits."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
             if User.objects.filter(student_no=student_no).exists():
                 return JsonResponse(
@@ -33,6 +56,8 @@ class Signup(APIView):
                 "student_no": student_no,
                 "email": email,
                 "phone_no": phone_no,
+                "gender": gender,
+                "hostel":hostel
             }
 
             serializer = userSerializer(data=data)
@@ -52,4 +77,3 @@ class Signup(APIView):
                 {"error": "An error occurred", "details": str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-         
