5d8d06953c1459b7253a58c32d44e1f3da418b69
Naitik <naitik12gupta05@gmail.com>
2024-12-09 23:37:01 +0530

    added static files

--- register/settings.py
+++ register/settings.py
@@ -10,11 +10,6 @@ dotenv.load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-STATIC_URL = '/static/'
-
-# Directory where all static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 
 
@@ -143,6 +138,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Directory where all static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
